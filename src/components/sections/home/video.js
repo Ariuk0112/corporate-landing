4900868b858cbcad2c1f31770600bec66f177d46
Ariuk0112 <ariunsanaaenkhbaatar12@gmail.com>
2023-03-26 19:35:15 +0800

    playbuton

--- src/components/sections/home/video.js
+++ src/components/sections/home/video.js
@@ -16,8 +16,16 @@ const Video = () => {
       >
         <div className="w-full h-full flex justify-center items-center">
           <a target="_blank" href="http://www.youtube.com/embed/lhl2BgFhrI0">
-            <div className="w-16 h-16 md:w-[90px] md:h-[90px] pulse">
-              <img src="/icons/play.png" className="w-full h-full" alt="play" />
+            <div className="w-16 h-16 md:w-[90px] md:h-[90px] pulse hover:scale-95 flex justify-center items-center text-white">
+              <div className="m-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="w-6 h-6 text-white">
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
             </div>
           </a>
         </div>
